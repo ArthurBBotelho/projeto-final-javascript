2828926621a2c471bc6c591a5ab15c9d0e7cb852
Arthur <emilioarthur19@gmail.com>
2023-08-09 13:33:04 -0300

    correrção de texto

--- pagina-principal/script.js
+++ pagina-principal/script.js
@@ -2,5 +2,5 @@ let nome = document.getElementById("nome")
 
 function inscrever () {
     console.log(nome.value)
-    alert("Parabéns, " + nome.value + ", sua inscrição efetuada com sucesso!")
+    alert("Parabéns, " + nome.value + ", sua inscrição foi efetuada com sucesso!")
 }
