144973c0f7d6cdb8ad7b32ccf204445f388c61ce
Emily Comin <108775557+emilycomin@users.noreply.github.com>
2023-08-24 23:29:59 -0300

    realizando o cadastro com DOM

--- pagina-login/scryptPaginaLogin.js
+++ pagina-login/scryptPaginaLogin.js
@@ -1,50 +1,60 @@
-document.getElementById("butonLogin").addEventListener("click", function(){ alert("Hello World!"); })
-
-
 //fazer a tela de cadastro igual a vitória do lig4, com fundo embassado....
 
 
 //REALIZANDO O CADSTRO DENTRO DO ARRAY
-//pedindo dados para o usari
+
 //função para criar a pessoa
 class adicionaPessoa {
     constructor(nome, senha) {
         //tornar tudo maiusculo
-        this.nome = nome.toUpperCase();
+        this.nome = nome
         //tornar numero inteiro
         this.senha = senha;
         // this.email = email;
     }
 }
-
 //adicionando uma pessoa e pedindo dados ao usuário
 let cadastro = [];
-//const novaPessoa = new adicionaPessoa(prompt("Digite Seu Nome para o Cadastro"), prompt("Digite Sua senha"), prompt("Digite seu E-mail"));
-let novaPessoa = new adicionaPessoa(prompt("Digite Seu Nome para o Cadastro"), prompt("Digite Sua senha"));
-if (novaPessoa !=""){    
-    alert("Cadastro realizado com sucesso!")
+
+//para fazer o login recuoeramos os dados no local storage com JSON.parse
+
+document.getElementById('getLoginButton').onclick = function btnCadastro(e) {
+    e.preventDefault();
+    //pegar dados do html
+    let userName = document.getElementById('inputUsername').value;
+    let userPassword = document.getElementById('inputPassword').value;
+    
+    //usar os dados para criar uma nova pessoa
+    let novaPessoa = new adicionaPessoa(userName, userPassword)
+    
+    //salvar a nova pessoa no array
+    cadastro.push(novaPessoa)
+    //salvar esse array no localstorage
+    localStorage.setItem("cadastroUsuário", JSON.stringify(cadastro))
+
+    //limpar formulário após cadastro
+    document.getElementById('inputUsername').value = ""
+    document.getElementById('inputPassword').value = ""
+
+    const cadastrado = document.createElement("p");
+    cadastrado.innerHTML = "Cadastro realizado com sucesso!"
+    document.getElementById('divCadastrado').appendChild(cadastrado);
+    console.log(userPassword)
+
+
+
 }
-    else{alert("Digite um nome");
-    }
-
-cadastro.push(novaPessoa)
-console.log(cadastro)
-
-
-//função para validar se usuário digitou os dados no cadastro
-
-//determina variaveis para captar usuario e senha agtravés de prompt
-let nomeUsuario = prompt("Digite seu nome para fazer login").toUpperCase()
-let inserirSenha = prompt("Digite a sua senha");
-
+    
+//pegar os dados salvos no localstorage e fazer verificação se o usuário existe e validar.
+//Aula 10
 //percorre o array cadastro e pega um dos itens do contrutor
-for (const nome of cadastro){ 
-        console.log(nome.nome)
-        console.log(nome.senha)
+// for (const nome of cadastro){ 
+//         console.log(nome.nome)
+//         console.log(nome.senha)
 
-        if((nome.nome === nomeUsuario) && (nome.senha === inserirSenha)) {
-            alert("Seus dados estão corretos")
-        }
-        else{ alert("Dados não são iguais")
-        }
-    }
+//         if((nome.nome === nomeUsuario) && (nome.senha === inserirSenha)) {
+//             alert("Seus dados estão corretos")
+//         }
+//         else{ alert("Dados não são iguais")
+//         }
+//     }
