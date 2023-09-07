063c40224bd475b10d855632db88e2c5ff4c3dd6
Emily Comin <108775557+emilycomin@users.noreply.github.com>
2023-09-06 22:49:10 -0300

    adicionado dialog para realizar o login do usuário na index

--- javaScript/scryptPaginaLogin.js
+++ javaScript/scryptPaginaLogin.js
@@ -1,10 +1,15 @@
 
 //pegar os dados salvos no localstorage e fazer verificação se o usuário existe e validar.
 //Aula 10
-//percorre o array cadastro e pega um dos itens do contrutor
-// for (const nome of cadastro){ 
-//         console.log(nome.nome)
-//         console.log(nome.senha)
+
+
+// pega o array de objetos do localStoreage
+
+//percorre o array de objetos procurando o mesmo texto do input
+
+//achou o texto igual e comparar se email e senha digitados são iguais
+
+//aparece as mensagens confirmando ou não.
 
 //         if((nome.nome === nomeUsuario) && (nome.senha === inserirSenha)) {
 //             alert("Seus dados estão corretos")
@@ -12,3 +17,19 @@
 //         else{ alert("Dados não são iguais")
 //         }
 //     }
+
+
+//div onde está o html do dialog
+let fazerLogin = document.getElementById('fazerLogin')
+
+document.getElementById('iconeLogin').onclick = function botaoLogin (e) {
+    e.preventDefault()
+    console.log("hello")
+    fazerLogin.showModal();
+}
+
+document.getElementById('btnFechar').onclick = function botaoFechar (e) {
+    e.preventDefault()
+    console.log("fechar")
+    fazerLogin.close();
+}
