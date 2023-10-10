2fa407773afb6a53b2cce1997481c69abddb403e
Emily Comin <108775557+emilycomin@users.noreply.github.com>
2023-10-09 22:09:47 -0300

    tirando códigos comentados e não usados

--- script.js
+++ script.js
@@ -42,60 +42,14 @@ function validEmail(email) {
     return false
 }
 
-// ----------------------------------------------------PRODUTO
-
-// const produtos = [{
-//     id: 3,
-//     imagem: "imagem/produtos/cadeira-comfy/cadeira-confy.jpg",
-//     produto: "Sofa Phillip",
-//     categoria: "sofa",
-//     preço: 4190.90,
-//     estoque: 3,
-//     vendido: false
-// },
-// {
-//     id: 4,
-//     imagem: "imagem/produtos/cadeira-quad/cadeira-quad.jpg",
-//     produto: "Cadeira Quad",
-//     categoria: "cadeira",
-//     preço: 1860.90,
-//     estoque: 15,
-//     vendido: false
-// }];
-
-// function organizaProdutos() {
-//     produtos.sort((a, b) => {
-//         if (a.produto > b.produto) {
-//             return 1;
-//         }
-//         if (a.produto < b.produto) {
-//             return -1;
-//         }
-//         return 0;
-//     })
-//     console.log(produtos)
-// }
-
-// organizaProdutos()
-
-// ------------------------------------------------------------------SEARCH
-
-
-// function search() {
-//     let buscaUser = prompt("Faça sua busca: ")
-//     const buscaProduto = produtos.filter((o) => o.produto.toUpperCase().includes(buscaUser.toUpperCase()))
-//     console.log(buscaProduto)
-// }
-
 //---------------------------------------------------------------NOME DE USUÁRIO LOGADO
+//pegando o nome do loocal storage
 let pegaNomeUsuario = JSON.parse(localStorage.getItem("salvarNomeUsuario"));
-
-console.log(pegaNomeUsuario)
+// caso não tenha nenhum nome salvo no local storage não faz nada
 if (pegaNomeUsuario == null) {
-
+//senão mostra o nome da pessoa
 } else {
     document.getElementById("nomeUsuario").innerHTML = "Olá, " + pegaNomeUsuario;
-    console.log(pegaNomeUsuario)
 }
 
 //-----------------------------------------------------------------abrir e fechar DIALOG
