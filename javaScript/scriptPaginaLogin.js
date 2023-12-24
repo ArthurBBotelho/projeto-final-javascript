261ccb3aabba8c127bdd35a9eec1f3a73dc3e760
Emily Comin <emilycominn@gmail.com>
2023-12-24 01:23:57 -0300

    revisnado código do carrinho

--- javaScript/scriptPaginaLogin.js
+++ javaScript/scriptPaginaLogin.js
@@ -1,55 +1,58 @@
 document.getElementById("buttonLogin").onclick = function getLogin(e) {
-    e.preventDefault();
-    //pegando elementos do HTML
-    let inputUserName = document.getElementById("inputUserName").value;
-    let inputPassword = document.getElementById("inputPassword").value;
-    
-//percorre o array de objetos procurando o mesmo texto do input
-   fetch('http://localhost:3000/cadastroUsuarios')
+  e.preventDefault();
+  //pegando elementos do HTML
+  let inputUserName = document.getElementById("inputUserName").value;
+  let inputPassword = document.getElementById("inputPassword").value;
+
+  //percorre o array de objetos procurando o mesmo texto do input
+  fetch("http://localhost:3000/cadastroUsuarios")
     .then((response) => response.json())
     .then((data) => {
-          for (let i = 0; i <= data.length; i++) {
-              console.log(inputUserName)
-              // validando se o usuário preencheu os inputs
-              if(inputUserName || inputPassword == ""){
-                  Swal.fire({
-                      position: 'center',
-                      icon: 'error',
-                      title: 'Preencha seus dados!',
-                      background: '#f1f4de',
-                      timer: 1000,
-                      showConfirmButton: false
-                  })
-              }
-              //validando o login com o db.JSON
-              if (inputUserName == data[i].email && inputPassword == data[i].senha) {
-                  //salvando o primeiro nome do usuário no localstorage para aparecer na index
-                  localStorage.setItem("salvarNomeUsuario", JSON.stringify(data[i].primeiroNome));
-                  Swal.fire({
-                      position: 'center',
-                      icon: 'success',
-                      title: 'Bem vindo!',
-                      background: '#f1f4de',
-                      timer: 1000,
-                      showConfirmButton: false,
-                  })
-                  //definindo um tempo para carregar a index, para que o usuário possa ver o SweetAlert
-                  setTimeout( () =>{
-                      window.location.href = "../index.html"
-                  }, 1000)
-                break
-               }
-               //validando se o usuário tem cadastro
-              if(inputUserName != data[i].email){
-                  Swal.fire({
-                      position: 'center',
-                      icon: 'error',
-                      title: 'E-mail não cadastrado!',
-                      background: '#f1f4de',
-                      timer: 1000,
-                      showConfirmButton: false
-                  });
-              }
-          } 
-    })
-}   
+      for (let i = 0; i <= data.length; i++) {
+        console.log(data);
+        // validando se o usuário preencheu os inputs
+        if (inputUserName || inputPassword == "") {
+          Swal.fire({
+            position: "center",
+            icon: "error",
+            title: "Preencha seus dados!",
+            background: "#f1f4de",
+            timer: 1000,
+            showConfirmButton: false,
+          });
+        }
+        //validando o login com o db.JSON
+        if (inputUserName == data[i].email && inputPassword == data[i].senha) {
+          //salvando o primeiro nome do usuário no localstorage para aparecer na index
+          localStorage.setItem(
+            "salvarNomeUsuario",
+            JSON.stringify(data[i].primeiroNome)
+          );
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Bem vindo!",
+            background: "#f1f4de",
+            timer: 1000,
+            showConfirmButton: false,
+          });
+          //definindo um tempo para carregar a index, para que o usuário possa ver o SweetAlert
+          setTimeout(() => {
+            window.location.href = "../index.html";
+          }, 1000);
+          break;
+        }
+        //validando se o usuário tem cadastro
+        if (inputUserName != data[i].email) {
+          Swal.fire({
+            position: "center",
+            icon: "error",
+            title: "E-mail não cadastrado!",
+            background: "#f1f4de",
+            timer: 1000,
+            showConfirmButton: false,
+          });
+        }
+      }
+    });
+};
--- produtos/produtos.js
+++ produtos/produtos.js
@@ -1,4 +1,5 @@
-const produtos = [{
+const produtos = [
+  {
     id: 1,
     imagem: '<img src="../produtos/cadeira-comfy/imagem/cadeira-comfy.png">',
     nome: "Cadeira Comfy",
@@ -6,9 +7,9 @@ const produtos = [{
     link: "cadeira-comfy",
     price: 1490,
     estoque: 16,
-    vendido: false
-},
-{
+    vendido: false,
+  },
+  {
     id: 2,
     imagem: '<img src="../produtos/sofa-outline/imagem/sofa-outline.png">',
     nome: "Sofa Outline",
@@ -16,9 +17,9 @@ const produtos = [{
     link: "sofa-outline",
     price: 6390,
     estoque: 8,
-    vendido: false
-},
-{
+    vendido: false,
+  },
+  {
     id: 3,
     imagem: '<img src="../produtos/sofa-phillip/imagem/sofa-phillip.png">',
     nome: "Sofa Phillip",
@@ -26,9 +27,9 @@ const produtos = [{
     link: "sofa-phillip",
     price: 4190,
     estoque: 3,
-    vendido: false
-},
-{
+    vendido: false,
+  },
+  {
     id: 4,
     imagem: '<img src="../produtos/cadeira-quad/imagem/cadeira-quad.png">',
     nome: "Cadeira Quad",
@@ -36,9 +37,9 @@ const produtos = [{
     link: "cadeira-quad",
     price: 1860,
     estoque: 15,
-    vendido: false
-},
-{
+    vendido: false,
+  },
+  {
     id: 5,
     imagem: '<img src="../produtos/banco-hans/imagem/banco-hans.png">',
     nome: "Banco Hans",
@@ -46,39 +47,42 @@ const produtos = [{
     link: "banco-hans",
     price: 780,
     estoque: 3,
-    vendido: false
-},
-{
+    vendido: false,
+  },
+  {
     id: 6,
-    imagem: '<img src="../produtos/escrivaninha-copa/imagem/escrivaninha-copa.png">',
+    imagem:
+      '<img src="../produtos/escrivaninha-copa/imagem/escrivaninha-copa.png">',
     nome: "Escrivaninha Copa",
     categoria: "escrivaninha",
     link: "escrivaninha-copa",
     price: 3560,
     estoque: 5,
-    vendido: false
-},
-{
+    vendido: false,
+  },
+  {
     id: 7,
-    imagem: '<img src="../produtos/mesa-apoio-cergle/imagem/mesa-apoio-cergle.png">',
+    imagem:
+      '<img src="../produtos/mesa-apoio-cergle/imagem/mesa-apoio-cergle.png">',
     nome: "Mesa de Apoio Cergle",
     categoria: "mesa",
     link: "mesa-apoio-cergle",
     price: 5130,
     estoque: 8,
-    vendido: false
-},
-{
+    vendido: false,
+  },
+  {
     id: 8,
-    imagem: '<img src="../produtos/mesa-apoio-creation/imagem/mesa-apoio-creation.png">',
+    imagem:
+      '<img src="../produtos/mesa-apoio-creation/imagem/mesa-apoio-creation.png">',
     nome: "Mesa de Apoio Creation",
     categoria: "mesa",
     link: "mesa-apoio-creation",
     price: 4890,
     estoque: 11,
-    vendido: false
-},
-{
+    vendido: false,
+  },
+  {
     id: 9,
     imagem: '<img src="../produtos/poltrona-silva/imagem/poltrona-silva.png">',
     nome: "Poltrona Silva",
@@ -86,158 +90,137 @@ const produtos = [{
     link: "poltrona-silva",
     price: 7550,
     estoque: 7,
-    vendido: false
-}
+    vendido: false,
+  },
 ];
-
+//--------------------------------------------------Exibindo o array de produtos na tela
 for (const produto of produtos) {
-    let container = document.createElement("div");
-    container.classList.add("produtosLayout")
+  let container = document.createElement("div");
+  container.classList.add("produtosLayout");
 
-    container.innerHTML = `<h3>${produto.nome}</h3>
+  container.innerHTML = `<h3>${produto.nome}</h3>
                             <a>${produto.imagem}</a>
                             <p class="price">${produto.price}</p>
                             <button id="adicionarCarrinho">Adicionar ao Carrinho</button>`;
-    document.getElementById("produtos").appendChild(container);
-
-}
-
-// função para criar um novo elemento genérico 
-/* const createCustomElement = (element, id, innerText, href) => {
-    const e = document.createElement(element);
-    e.id = id;
-    e.innerText = innerText;
-    if (element == 'button') {
-      e.addEventListener('click', addProductsToCart);
-    }
-    return e;
-  };
-*/
-
-// --------------------------------------------------------------------- ABRIR O MODAL CARRINHO
-
-let carrinhoModal = document.getElementById("carrinhoModal")
-
-document.getElementById('iconeCarrinho').onclick = function botaoCarrinho(e) {
-    e.preventDefault()
-    console.log("Usuário abriu carrinho")
-    carrinhoModal.showModal();
-}
-
-// --------------------------------------------------------------------- FECHAR O MODAL CARRINHO
-
-document.getElementById('btnFechar').onclick = function botaoFechar(e) {
-    e.preventDefault()
-    console.log("Usuário fechou carrinho")
-    carrinhoModal.close();
-}
-
-window.onload = () => {
-    carregarCarrinho()
+  document.getElementById("produtos").appendChild(container);
 }
 
 // --------------------------------------------------------------------- CARRINHO
 
-const carrinho = []
+const carrinho = [];
 
-let arrayCarrinho = document.querySelectorAll("#adicionarCarrinho")
-console.log(arrayCarrinho)
-let dialogCarrinho = document.querySelector("#carrinhoModal")
+let arrayCarrinho = document.querySelectorAll("#adicionarCarrinho");
+// console.log(arrayCarrinho);
+let dialogCarrinho = document.querySelector("#carrinhoModal");
 
-
-//---------------------------------------------------------------------- CONTEÚDO QUE SERÁ EXIBIDO DENTRO DO DIALOG CARRINHO
+//----------------------------------------------------- CONTEÚDO QUE SERÁ EXIBIDO DENTRO DO DIALOG CARRINHO
 
 function renderCarrinho(nome, price, imagem) {
-    let nomeProduto = document.createElement("div")
-    nomeProduto.classList.add("produtosCarrinho")
-    let carrinhoVazio = document.getElementById('carrinhoVazio')
+  let nomeProduto = document.createElement("div");
+  nomeProduto.classList.add("produtosCarrinho");
+  let carrinhoVazio = document.getElementById("carrinhoVazio");
 
-    nomeProduto.innerHTML = `
+  nomeProduto.innerHTML = `
     <p>${imagem}</p>
     <p>${nome}</p>
     <p>Preço: ${price}</p>
     <hr>
-    `
+    `;
 
-    if (carrinhoVazio) {
-        carrinhoVazio.remove()
-    }
-    document.getElementById("produtosCarrinho").appendChild(nomeProduto)
+  if (carrinhoVazio) {
+    carrinhoVazio.remove();
+  }
+  document.getElementById("produtosCarrinho").appendChild(nomeProduto);
 }
 
 function somaResultado() {
-    let resultadoCarrinho = 0 // Valor do carrinho começa zerado
-    let resultadoCarrinhoFeito = document.querySelector(".resultadoCarrinhoRender")
+  let resultadoCarrinho = 0; // Valor do carrinho começa zerado
+  let resultadoCarrinhoFeito = document.querySelector(
+    ".resultadoCarrinhoRender"
+  );
 
-    if (resultadoCarrinhoFeito) {
-        resultadoCarrinhoFeito.remove() // Remove mensagem "Seu carrinho está vazio"
-    }
+  if (resultadoCarrinhoFeito) {
+    resultadoCarrinhoFeito.remove(); // Remove mensagem "Seu carrinho está vazio"
+  }
 
-    for (const price of carrinho) {
-        resultadoCarrinho += parseInt(price.price) // Soma o preço dos produtos
-    }
-    console.log("Valor do Carrinho: " + resultadoCarrinho)
+  for (const price of carrinho) {
+    resultadoCarrinho += parseInt(price.price); // Soma o preço dos produtos
+  }
 
-    let resultadoCarrinhoRender = document.createElement("div") // Cria a div que recebe o valor do carrinho com a class resultadoCarrinhoRender
-    resultadoCarrinhoRender.classList.add("resultadoCarrinhoRender")
-    resultadoCarrinhoRender.innerHTML = `
+  let resultadoCarrinhoRender = document.createElement("div"); // Cria a div que recebe o valor do carrinho com a class resultadoCarrinhoRender
+  resultadoCarrinhoRender.classList.add("resultadoCarrinhoRender");
+  resultadoCarrinhoRender.innerHTML = `
     <h3>Valor do Carrinho: ${resultadoCarrinho}</h3>
-    `
+    `;
 
-    document.getElementById("produtosCarrinho").appendChild(resultadoCarrinhoRender) // Renderiza o resultado da soma do carrinho para o HTML 
+  document
+    .getElementById("produtosCarrinho")
+    .appendChild(resultadoCarrinhoRender); // Renderiza o resultado da soma do carrinho para o HTML
 }
 // --------------------------------------------------------------------- FUNÇÃO ADICIONAR PRODUTOS NO CARRINHO
 
 function adicionarProduto() {
-    const nome = this.parentElement.childNodes[0].innerText
+  const nome = this.parentElement.childNodes[0].innerText;
 
-    const price = this.parentElement.childNodes[4].innerText
+  const price = this.parentElement.childNodes[4].innerText;
 
-    const imagem = this.parentElement.childNodes[2].innerHTML
-    carrinho.push({ nome, price, imagem })
-    console.log(carrinho)
-    
-    renderCarrinho(nome, price, imagem)
-    somaResultado()
-    //pega a div no html para mostrar a quatidade do array do carrinho de produtos
-    document.getElementById("mostraQuantidade").innerHTML = carrinho.length
-    carrinhoModal.showModal();
-    setTimeout(() => {
-        carrinhoModal.close();
-    }, 1000);
-    localStorage.setItem("carrinhoSalvo", JSON.stringify(carrinho));
+  const imagem = this.parentElement.childNodes[2].innerHTML;
+  carrinho.push({ nome, price, imagem });
+
+  renderCarrinho(nome, price, imagem);
+  somaResultado();
+
+  //pega a div no html para mostrar a quatidade do array do carrinho de produtos
+  carrinhoModal.showModal();
+  setTimeout(() => {
+    carrinhoModal.close();
+  }, 1000);
+  localStorage.setItem("carrinhoSalvo", JSON.stringify(renderCarrinho));
 }
+// --------------------------------------------------------------------- FUNÇÃO PARA SOMAR PRODUTOS ADICIONADOS NO CARRINHO
 
+for (i = 0; i < arrayCarrinho.length; i++) {
+  arrayCarrinho[i].addEventListener("click", adicionarProduto);
+}
 
 //---------------------------------------------------------------------- ADICIONAR LOCAL STORAGE
 
 function carregarCarrinho() {
-    const armazenados = JSON.parse(localStorage.getItem("carrinhoSalvo"));
+  const armazenados = JSON.parse(localStorage.getItem("carrinhoSalvo"));
 
-    for (const produtos of armazenados) {
-        renderCarrinho(produtos.nome, produtos.price, produtos.imagem)
-        carrinho.push({ nome: produtos.nome, price: produtos.price, imagem: produtos.imagem })
-    }
-    somaResultado()
+  for (const produtos of armazenados) {
+    renderCarrinho(produtos.nome, produtos.price, produtos.imagem);
+    carrinho.push({
+      nome: produtos.nome,
+      price: produtos.price,
+      imagem: produtos.imagem,
+    });
+  }
+  somaResultado();
 }
 // --------------------------------------------------------------------- FUNÇÃO LIMPAR STORAGE E ITENS NO CARRINHO
 
-let limparCarrinho = document.getElementById("limparCarrinho")
+document.getElementById("limparCarrinho").onclick = function limparCarrinho() {
+  document.getElementById("produtosCarrinho").innerHTML = carrinho.splice(
+    0,
+    carrinho.length
+  );
+  localStorage.removeItem("carrinhoSalvo");
+};
+// --------------------------------------------------------------------- ABRIR O MODAL CARRINHO
 
-limparCarrinho.addEventListener('click', limparProdutos)
+let carrinhoModal = document.getElementById("carrinhoModal");
 
-function limparProdutos() {
-    // document.querySelector("#produtosCarrinho").remove()
-    document.getElementById("mostraQuantidade").innerHTML= " "
-    document.getElementById("produtosCarrinho").innerHTML= " "
-    localStorage.removeItem("carrinhoSalvo");
-    carrinho = []
-    
-}
+document.getElementById("iconeCarrinho").onclick = function botaoCarrinho(e) {
+  e.preventDefault();
+  console.log("Usuário abriu carrinho");
+  carrinhoModal.showModal();
+};
 
-// --------------------------------------------------------------------- FUNÇÃO PARA SOMAR PRODUTOS ADICIONADOS NO CARRINHO
+// --------------------------------------------------------------------- FECHAR O MODAL CARRINHO
 
-for (i = 0; i < arrayCarrinho.length; i++) {
-    arrayCarrinho[i].addEventListener('click', adicionarProduto)
-}
-
+document.getElementById("btnFechar").onclick = function botaoFechar(e) {
+  e.preventDefault();
+  console.log("Usuário fechou carrinho");
+  carrinhoModal.close();
+};
